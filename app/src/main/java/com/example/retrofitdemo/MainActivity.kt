895009563c46575c172e85ced516df7eca91c06d
lakpahana <agslakpahana@gmail.com>
2022-08-15 23:35:56 +0530

    query and patha parameters added

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.retrofitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
@@ -10,6 +11,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,8 +19,23 @@ class MainActivity : AppCompatActivity() {
         val retService: AlbumService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
+
+        //path parameters example
+
+        val pathResponse : LiveData<Response<AlbumsItem>> = liveData {
+            val response = retService.getAlbum(5)
+            emit(response)
+        }
+
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext,title,Toast.LENGTH_SHORT)
+        })
+
         val  responseLiveData:LiveData<Response<Albums>> = liveData{
-            val response = retService.getAlbums()
+//            val response = retService.getAlbums()
+            val response = retService.getSortedAlbums(3)
+
             emit(response)
         }
         responseLiveData.observe(this, Observer {
